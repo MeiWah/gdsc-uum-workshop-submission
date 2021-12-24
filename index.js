a427c4a9a4bd24d24364cbd5a7086a464a62b6e4
MeiWah <96556629+MeiWah@users.noreply.github.com>
2021-12-25 00:55:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+ //This is my first time using Jawascript Code
+
+ let name="Hafizi"
+ let age= 26 ;
+ console.log(name);
+
+ let person 
+    name = "Hafizi",
+    age = 26
+ console.log(person);
+    
+let selectedcolors = ['red','purple']
+console.log(selectedcolors)
+
+
+function greet (name) {
+ console.log ('hello world');
+}
+
+//performing a task
+function greet(name) {
+console.log('Hello' +name);
+}
